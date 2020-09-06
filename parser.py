03c620ca591fced81e078d6504d6e45180007c79
davidbarat <dav.barat@gmail.com>
2020-09-06 02:37:03 +0200

    last commit

--- parser.py
+++ parser.py
@@ -7,12 +7,12 @@ from stop_words import stops
 
 class Parser():
 
-
     def delete_stopwords(self, search_post):
 
         self.clean_search_post = ''
         self.list_search_post = search_post.split(" ")
-        self.clean_search_post = [word for word in self.list_search_post if word.lower() not in stops]
-        self.clean_search_post = [i.replace("'", "+") for i in self.clean_search_post]
-        print(self.clean_search_post)
+        self.clean_search_post = [
+            word for word in self.list_search_post if word.lower() not in stops]
+        self.clean_search_post = [
+            i.replace("'", "+") for i in self.clean_search_post]
         return(self.clean_search_post)
